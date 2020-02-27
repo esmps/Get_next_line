b979bc60767a1210ee6f4eb0dcbf83107d2c82ab
Emma Pines-schwartz <epines-s@e1z2r3p12.42.us.org>
2020-02-26 19:41:42 -0800

    updated and compiles but does not function properly

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,60 @@
 /*   By: epines-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:12:39 by epines-s          #+#    #+#             */
-/*   Updated: 2020/02/25 20:16:52 by epines-s         ###   ########.fr       */
+/*   Updated: 2020/02/26 19:28:43 by epines-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.c"
+#include "get_next_line.h"
 
-char	pointtothis();
+static char		*new_line(char *storage_fd)
 {
+	char	*newline;
+	int		i;
+
+	i = 0;
+	while (storage_fd[i] != '\0' && storage_fd[i] != '\n')
+		i++;
+	newline = ft_strsub(storage_fd, 0, i);
+	newline[i] = '\0';
+	return (newline);
 }
 
-
-int		get_next_line(const int fd, char **line)
+static char		*store_this(char *storage_fd)
 {
-	static char	*temp[1023];
-	char		buf[BUFF_SIZE + 1];
-	int			readres;
+	char	*res;
+	int		i;
 
+	i = 0;
+	while (storage_fd[i] != '\0' && storage_fd[i] != '\n')
+		i++;
+	res = &storage_fd[i + 1];
+	return (res);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static char	*storage[1024];
+	char		buf[BUFF_SIZE + 1];
+	char		*temp;
+	int			readres;
 
 	if (line == NULL || fd < 0 || read(fd, buf, 0) < 0)
 		return (-1);
-	while (readres = read(fd, buf, BUFF_SIZE) > 0)
+	while ((readres = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-
-
+		if (!storage[fd])
+			storage[fd] = ft_strnew(0);
+		buf[readres] = '\0';
+		temp = ft_strjoin(storage[fd], buf);
+		free(storage[fd]);
+		storage[fd] = temp;
+		if (ft_strchr(storage[fd], '\n'))
+			break ;
 	}
-	*line = pointtothis( );
-	temp = therestofthestring();
+	*line = new_line(storage[fd]);
+	storage[fd] = store_this(storage[fd]);
+	if (storage[fd] && *storage[fd] == '\0')
+		return (0);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,21 @@
 /*   By: epines-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:15:31 by epines-s          #+#    #+#             */
-/*   Updated: 2020/02/25 20:43:41 by epines-s         ###   ########.fr       */
+/*   Updated: 2020/02/26 19:15:42 by epines-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <unistd.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include "libft/libft.h"
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 100
 
 int		get_next_line(const int fd, char **line);
 
 #endif
-
