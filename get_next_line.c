88352ab3b90bf4c07edfc26cf71c79ec7db0af99
Emma Pines-Schwartz <emma.pines.s@gmail.com>
2020-03-20 13:55:10 -0700

    updated due to new cirriculum

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,39 @@
 /*   By: epines-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/22 13:12:39 by epines-s          #+#    #+#             */
-/*   Updated: 2020/03/02 11:27:08 by epines-s         ###   ########.fr       */
+/*   Updated: 2020/03/03 19:44:06 by epines-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char		*line_to_read(char *storage_fd)
+int		ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (s[i] == c)
+		return ((char *)(s + i));
+	return (NULL);
+}
+
+char	*line_to_read(char *storage_fd)
 {
 	char	*newline;
 	int		i;
@@ -20,17 +46,17 @@ static char		*line_to_read(char *storage_fd)
 	i = 0;
 	newline = NULL;
 	if (!storage_fd)
-		return (ft_strnew(0));
+		return (ft_memalloc(0));
 	while (storage_fd[i] != '\0' && storage_fd[i] != '\n')
 		i++;
 	if (i == 0 && storage_fd[i] == '\0')
-		return (ft_strnew(0));
-	newline = ft_strsub(storage_fd, 0, i);
+		return (ft_memalloc(0));
+	newline = ft_substr(storage_fd, 0, i);
 	newline[i] = '\0';
 	return (newline);
 }
 
-static char		*store_the_rest(char *storage_fd)
+char	*store_the_rest(char *storage_fd)
 {
 	char	*res;
 	int		i;
@@ -42,13 +68,13 @@ static char		*store_the_rest(char *storage_fd)
 		i++;
 	if (i == 0 && storage_fd[i] == '\0')
 		return (NULL);
-	res = ft_strsub(storage_fd, (i + 1), \
-			((ft_strlen(storage_fd) + 1) - (i + 1)));
+	res = ft_substr(storage_fd, \
+			(i + 1), ((ft_strlen(storage_fd) + 1) - (i + 1)));
 	free(storage_fd);
 	return (res);
 }
 
-int				get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
 	static char	*storage[1024];
 	char		buf[BUFF_SIZE + 1];
@@ -60,7 +86,7 @@ int				get_next_line(const int fd, char **line)
 	while ((readres = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		if (!storage[fd])
-			storage[fd] = ft_strnew(0);
+			storage[fd] = ft_memalloc(0);
 		buf[readres] = '\0';
 		temp = ft_strjoin(storage[fd], buf);
 		free(storage[fd]);
